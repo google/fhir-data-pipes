8ce6f502e1969015787f8d102da1662b5e130d2b
Bashir Sadjad <bashir.sadjad@gmail.com>
2020-05-26 01:42:47 -0400

    added json parsing of event content

--- src/main/java/org/openmrs/FeedConsumer.java
+++ src/main/java/org/openmrs/FeedConsumer.java
@@ -4,29 +4,35 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
+import org.ict4h.atomfeed.client.service.FeedClient;
 import org.openmrs.module.atomfeed.client.AtomFeedClient;
 import org.openmrs.module.atomfeed.client.AtomFeedClientFactory;
 
 public class FeedConsumer {
-  private String feedBaseUrl;
-  private AtomFeedClient feedClient;
-  private List<String> categories;
+  private List<AtomFeedClient> feedClients = new ArrayList<>();
 
   FeedConsumer(String feedBaseUrl) throws URISyntaxException {
-    this.feedBaseUrl = feedBaseUrl;
-    feedClient = AtomFeedClientFactory.createClient(new FhirEventWorker());
-    // TODO check if this can be set by configuring above factory call.
-    URI feedUri = new URI(feedBaseUrl);
-    feedClient.setUri(feedUri);
-    categories = new ArrayList<>();
+    List<String> categories = new ArrayList<>();
+    categories.add("visit");
+    /* TODO add these and anything else needed, after debugging
     categories.add("patient");
     categories.add("encounter");
     categories.add("observation");
-    categories.add("visit");
+     */
+    for (String c : categories) {
+      AtomFeedClient feedClient = AtomFeedClientFactory
+          .createClient(new FhirEventWorker(feedBaseUrl));
+      // TODO check if this can be set by configuring above factory call & finalize the feed number.
+      URI feedUri = new URI(feedBaseUrl + "/ws/atomfeed/" + c + "/1");
+      feedClient.setUri(feedUri);
+      feedClients.add(feedClient);
+    }
   }
 
   public void listen() {
-    feedClient.process();
+    for (AtomFeedClient client : feedClients) {
+      client.process();
+    }
   }
 
 }
--- src/main/java/org/openmrs/FhirEventWorker.java
+++ src/main/java/org/openmrs/FhirEventWorker.java
@@ -1,5 +1,9 @@
 package org.openmrs;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonParser;
 import org.ict4h.atomfeed.client.domain.Event;
 import org.ict4h.atomfeed.client.service.EventWorker;
 import org.slf4j.Logger;
@@ -7,10 +11,32 @@ import org.slf4j.LoggerFactory;
 
 public class FhirEventWorker implements EventWorker {
   private static final Logger log = LoggerFactory.getLogger(FhirEventWorker.class);
+  private String feedBaseUrl;
+
+  FhirEventWorker(String feedBaseUrl) {
+    this.feedBaseUrl = feedBaseUrl;
+  }
 
   @Override
   public void process(Event event) {
     log.info("In process for event: " + event);
+    String content = event.getContent();
+    if (content == null || "".equals(content)) {
+      log.warn("No content in event: " + event);
+      return;
+    }
+    Gson gson = new Gson();
+    try {
+      JsonObject jsonObj = JsonParser.parseString(content).getAsJsonObject();
+      String fhirResource = jsonObj.get("fhir").getAsString();
+      if (fhirResource == null || "".equals(fhirResource)) {
+        log.info("Skipping non-FHIR event " + event);
+        return;
+      }
+      log.info("FHIR resource is: " + fhirResource);
+    } catch (JsonParseException e) {
+      log.error(String.format("Error parsing content %s of event %s", content, event.toString()));
+    }
   }
 
   @Override
