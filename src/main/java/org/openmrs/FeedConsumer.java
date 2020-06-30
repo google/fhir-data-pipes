3abbe9c5be4dee019056bedb193317cd70d0d6c9
Bashir Sadjad <bashir.sadjad@gmail.com>
2020-06-30 16:13:55 -0400

    comment clean-up

--- src/main/java/org/openmrs/FeedConsumer.java
+++ src/main/java/org/openmrs/FeedConsumer.java
@@ -17,7 +17,6 @@ package org.openmrs;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
