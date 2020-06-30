e7d6006c7e60777f916c5aeb758b9136dcb7cd63
Bashir Sadjad <bashir.sadjad@gmail.com>
2020-06-30 15:27:22 -0400

    minor test comments

--- src/test/java/org/openmrs/FhirEventWorkerTest.java
+++ src/test/java/org/openmrs/FhirEventWorkerTest.java
@@ -5,9 +5,10 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * TODO: Add tests for the FhirEventWorker class; this requires refactoring FhirEventWorker and do
+ * proper dependency injection for all external service calls that it does.
  */
-public class AppTest 
+public class FhirEventWorkerTest
 {
     /**
      * Rigorous Test :-)
