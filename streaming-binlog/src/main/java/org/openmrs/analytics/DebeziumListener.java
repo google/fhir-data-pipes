4a0d7dde1d7577b9c9f857df915cfe28fa8b056d
cliff <gitacliff48@gmail.com>
2021-02-04 23:14:47 +0300

    Move:refactoring DebeziumArgs

--- streaming-binlog/src/main/java/org/openmrs/analytics/DebeziumListener.java
+++ streaming-binlog/src/main/java/org/openmrs/analytics/DebeziumListener.java
@@ -126,29 +126,14 @@ public class DebeziumListener extends RouteBuilder {
 		@Parameter(names = { "--databaseHostName" }, description = "Host name on which the source database runs")
 		public String databaseHostName = "localhost";
 		
-		@Parameter(names = { "--databasePort" }, description = "Port of the source database")
-		public Integer databasePort = 3306;
-		
 		@Parameter(names = { "--databaseUser" }, description = "User name of the host Database")
 		public String databaseUser = "root";
 		
 		@Parameter(names = { "--databasePassword" }, description = "Passowrd for the user of the host Database")
 		public String databasePassword = "debezium";
 		
-		@Parameter(names = { "--databaseName" }, description = "Name Database")
-		public String databaseName = "mysql";
-		
 		@Parameter(names = { "--databaseSchema" }, description = "Name the Schema")
 		public String databaseSchema = "openmrs";
-		
-		@Parameter(names = { "--databaseServerId" }, description = "Server Id of the source database")
-		public Integer databaseServerId = 223344;
-		
-		@Parameter(names = { "--databaseOffsetStorage" }, description = "Database OffsetStorage setting")
-		public String databaseOffsetStorage = "data/offset.dat";
-		
-		@Parameter(names = { "--databaseHistory" }, description = "replacing Offset by History")
-		public String databaseHistory = "data/dbhistory.dat";
 		
 		@Parameter(names = { "--snapshotMode" }, description = "criteria for running DB snapshot, options include: "
 		        + "when_needed, schema_only, initial, never, e.t.c. To read events from day 0 of db data-entry, use 'initial'. "
