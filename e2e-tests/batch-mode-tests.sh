8f3469c93bacabc19d593b153e204dab6117979e
jacinta254 <jecihjoy@gmail.com>
2021-02-05 11:32:31 +0300

    PR feedbacks

--- e2e-tests/batch-mode-tests.sh
+++ e2e-tests/batch-mode-tests.sh
@@ -1,54 +1,52 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 print_prefix="E2E: BATCH MODE TEST:"
-echo "START OF BATCH MODE TEST"
+echo "${print_prefix} START OF BATCH MODE TEST"
 echo "${print_prefix} STARTING OPENMRS SERVER"
-docker-compose -f openmrs-compose.yaml up -d
-statuscode=$(curl -o /dev/null --head -w "%{http_code}" -L -X GET -u admin:Admin123 http://localhost:8099/openmrs/ws/rest/v1/visittype)
-until [[ ${statuscode} -eq 200 ]]; do
-      sleep 40s;
+docker-compose -f openmrs-compose.yaml up -d --remove-orphans
+openmrs_start_wait_time=0
+contenttype=$(curl -o /dev/null --head -w "%{content_type}\n" -X GET -u admin:Admin123 --connect-timeout 5 --max-time 20 http://localhost:8099/openmrs/ws/rest/v1/visittype 2>/dev/null | cut -d ";" -f 1)
+until [[ ${contenttype} == "application/json" ]]; do
+      sleep 60s;
       echo "${print_prefix} WAITING FOR OPENMRS SERVER TO START"
-      statuscode=$(curl -o /dev/null --head -w "%{http_code}" -L -X GET -u admin:Admin123 http://localhost:8099/openmrs/ws/rest/v1/visittype)
+      contenttype=$(curl -o /dev/null --head -w "%{content_type}\n" -X GET -u admin:Admin123 --connect-timeout 5 --max-time 20 http://localhost:8099/openmrs/ws/rest/v1/visittype 2>/dev/null | cut -d ";" -f 1)
+      ((openmrs_start_wait_time+=1))
+      if [[ ${openmrs_start_wait_time} == 20 ]]; then
+        echo "${print_prefix} TERMINATING TEST AS OPENMRS TOOK TOO LONG TO START"
+        exit 1
+      fi
 done
 echo "${print_prefix} OPENMRS SERVER STARTED SUCCESSFULLY"
-echo "${print_prefix} BUILD THE ANALYTICS PROJECT"
-mvn clean install -DskipTests=true 
-rm -rf /tmp/analytics-tests
-mktemp -d /tmp/analytics-tests
-echo "${print_prefix} RUN BATCH MODE WITH PARQUET SYNC"
-java -cp batch/target/fhir-batch-etl-bundled-0.1.0-SNAPSHOT.jar org.openmrs.analytics.FhirEtl --openmrsServerUrl=http://localhost:8099/openmrs --openmrsUserName=admin --openmrsPassword=Admin123  --searchList=Patient,Encounter,Observation --batchSize=20 --targetParallelism=20 --fileParquetPath=/tmp/analytics-tests/
-if [[ -z $(ls /tmp/analytics-tests/Patient) ]]; then
+echo "${print_prefix} BUILDING THE ANALYTICS PROJECT"
+echo "mvn clean install -DskipTests=true"
+mvn clean install -DskipTests=true
+test_dir=$(mktemp -d --tmpdir -t analytics_tests__XXXXXX)
+echo "${print_prefix} PARQUET FILES WILL BE WRITTEN INTO $test_dir DIRECTORY"
+echo "${print_prefix} RUNNING BATCH MODE WITH PARQUET SYNC"
+echo "java -cp $(echo batch/target/fhir-batch-etl-bundled-*.jar | cut -d " " -f 1) org.openmrs.analytics.FhirEtl --openmrsServerUrl=http://localhost:8099/openmrs --openmrsUserName=admin --openmrsPassword=Admin123  --searchList=Patient,Encounter,Observation --batchSize=20 --targetParallelism=20 --fileParquetPath='${test_dir}'/"
+java -cp $(echo batch/target/fhir-batch-etl-bundled-*.jar | cut -d " " -f 1) org.openmrs.analytics.FhirEtl --openmrsServerUrl=http://localhost:8099/openmrs --openmrsUserName=admin --openmrsPassword=Admin123  --searchList=Patient,Encounter,Observation --batchSize=20 --targetParallelism=20 --fileParquetPath="${test_dir}"/
+if [[ -z $(ls "${test_dir}"/Patient) ]]; then
   echo "${print_prefix} DATA NOT SYNCED TO PARQUET FILES"
-  exit 0
+  exit 1
 fi
-echo "${print_prefix} PARQUET FILES SYNCED IN /tmp/analytics-tests DIRECTORY"​
-current_path=`pwd`
+echo "${print_prefix} PARQUET FILES SYNCED TO ${test_dir​}"
+current_path=$(pwd)
 echo "${print_prefix} COPYING PARQUET TOOLS JAR FILE TO ROOT"
-cp e2e-tests/parquet-tools-1.11.1.jar /tmp/analytics-tests/
+cp e2e-tests/parquet-tools-1.11.1.jar "${test_dir}"
 if ! [[ $(command -v jq) ]]; then
-    echo "INSTALLING JSON COMMAND-LINE PROCESSOR, JQ" >&2
-    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        echo "OSTYPE: linux"
-        sudo apt install jq
-    elif [[ "$OSTYPE" == "darwin"* ]]; then
-        echo "OSTYPE: Mac OSX"
-        brew install jq
-    fi
+    echo "${print_prefix} COULD NOT INSTALL JQ, INSTALL MANUALLY TO CONTINUE RUNNING THE TEST"
+    exit 1
 fi
-
-if ! [[ $(command -v jq) ]]; then
-  echo "COULD NOT INSTALL JQ, INSTALL MANUALLY TO CONTINUE RUNNING THE TEST" >&2
-  brew install jq
-fi
-cd /tmp/analytics-tests
+cd "${test_dir}"
 mkdir omrs
 echo "${print_prefix} Finding number of patients, encounters and obs in openmrs server"
-curl -L -X GET -u admin:Admin123 http://localhost:8099/openmrs/ws/fhir2/R4/Patient/ >> ./omrs/patients.json
+curl -L -X GET -u admin:Admin123 --connect-timeout 5 --max-time 20 http://localhost:8099/openmrs/ws/fhir2/R4/Patient/ 2>/dev/null >> ./omrs/patients.json
 total_test_patients=$(jq '.total' ./omrs/patients.json)
 echo "${print_prefix} Total openmrs test patients ---> ${total_test_patients}"
-curl -L -X GET -u admin:Admin123 http://localhost:8099/openmrs/ws/fhir2/R4/Encounter/ >> ./omrs/encounters.json
+curl -L -X GET -u admin:Admin123 --connect-timeout 5 --max-time 20 http://localhost:8099/openmrs/ws/fhir2/R4/Encounter/ 2>/dev/null >> ./omrs/encounters.json
 total_test_encounters=$(jq '.total' ./omrs/encounters.json)
 echo "${print_prefix} Total openmrs test encounters ---> ${total_test_encounters}"
-curl -L -X GET -u admin:Admin123 http://localhost:8099/openmrs/ws/fhir2/R4/Observation/ >> ./omrs/obs.json
+curl -L -X GET -u admin:Admin123 --connect-timeout 5 --max-time 20 http://localhost:8099/openmrs/ws/fhir2/R4/Observation/ 2>/dev/null >> ./omrs/obs.json
 total_test_obs=$(jq '.total' ./omrs/obs.json)
 echo "${print_prefix} Total openmrs test obs ---> ${total_test_obs}"
 echo "${print_prefix} Counting number of patients, encounters and obs sinked to parquet files"
@@ -58,40 +56,52 @@ total_encounters_streamed=$(java -jar ./parquet-tools-1.11.1.jar rowcount ./Enco
 echo "${print_prefix} Total encounters synched to parquet ---> ${total_encounters_streamed}"
 total_obs_streamed=$(java -jar ./parquet-tools-1.11.1.jar rowcount ./Observation/ | awk '{print $3}')
 echo "${print_prefix} Total obs synched to parquet ---> ${total_obs_streamed}"
-if [[ (${total_patients_streamed} == ${total_test_patients}) && (${total_encounters_streamed} == ${total_test_encounters}) && ($total_obs_streamed == ${total_test_obs})]] ;
+if [[ ${total_patients_streamed} == ${total_test_patients} && ${total_encounters_streamed} == ${total_test_encounters} && $total_obs_streamed == ${total_test_obs} ]] ;
 then
   echo "${print_prefix} BATCH MODE WITH PARQUET SYNC EXECUTED SUCCESSFULLY"
 else
   echo "${print_prefix} BATCH MODE WITH PARQUET SYNC TEST FAILED"
+  exit 1
 fi
-cd $current_path
-echo "RUN BATCH MODE WITH FHIR SYNC"
-echo "STARTING FHIR SERVER"
+cd "${current_path}"
+echo "${print_prefix} RUNNING BATCH MODE WITH FHIR SYNC"
+echo "${print_prefix} STARTING FHIR SINK SERVER"
 docker-compose -f sink-compose.yml up -d
-fhir_server_status_code=$(curl -o /dev/null --head -w "%{http_code}" -L -X GET -u hapi:hapi http://localhost:8098/fhir/Observation)
-echo "fhir_server_status_code ---> ${fhir_server_status_code}"
+fhir_server_start_wait_time=0
+fhir_server_status_code=$(curl -o /dev/null --head -w "%{http_code}" -L -X GET -u hapi:hapi --connect-timeout 5 --max-time 20 http://localhost:8098/fhir/Observation 2>/dev/null)
 until [[ ${fhir_server_status_code} -eq 200 ]]; do
-      sleep 20s;
-      echo 'WAITING FOR FHIR SERVER TO START'
-      fhir_server_status_code=$(curl -o /dev/null --head -w "%{http_code}" -L -X GET -u hapi:hapi http://localhost:8098/fhir/Observation)
+      sleep 1s;
+      echo "${print_prefix} WAITING FOR FHIR SERVER TO START"
+      fhir_server_status_code=$(curl -o /dev/null --head -w "%{http_code}" -L -X GET -u hapi:hapi --connect-timeout 5 --max-time 20 http://localhost:8098/fhir/Observation 2>/dev/null)
+      ((fhir_server_start_wait_time+=1))
+      if [[ fhir_server_start_wait_time == 10 ]]; then
+        echo "${print_prefix} TERMINATING AS FHIR SERVER TOOK TOO LONG TO START"
+        exit 1
+      fi
 done
 echo "${print_prefix} FHIR SERVER STARTED SUCCESSFULLY"
+echo "mvn exec:java -pl batch
+ -Dexec.args=--openmrsServerUrl=http://localhost:8099/openmrs --openmrsUserName=admin --openmrsPassword=Admin123
+   --searchList=Patient --batchSize=20 --targetParallelism=20
+   --fhirSinkPath=http://localhost:8098/fhir
+   --sinkUserName=hapi --sinkPassword=hapi"
+
 mvn exec:java -pl batch \
  "-Dexec.args=--openmrsServerUrl=http://localhost:8099/openmrs --openmrsUserName=admin --openmrsPassword=Admin123 \
    --searchList=Patient --batchSize=20 --targetParallelism=20 \
    --fhirSinkPath=http://localhost:8098/fhir \
    --sinkUserName=hapi --sinkPassword=hapi"
-
-cd /tmp/analytics-tests
+cd "${test_dir}"
 mkdir fhir
 echo "${print_prefix} Counting number of patients, encounters and obs sinked to fhir files"
-curl -L -X GET -u hapi:hapi http://localhost:8098/fhir/Patient/ >> ./fhir/patients.json
+curl -L -X GET -u hapi:hapi --connect-timeout 5 --max-time 20 http://localhost:8098/fhir/Patient/?_summary=count 2>/dev/null >> ./fhir/patients.json
 total_patients_sinked_fhir=$(jq '.total' ./fhir/patients.json)
-echo "Total patients sinked to fhir ---> $total_patients_sinked_fhir"
-if [[ ($total_patients_sinked_fhir == $total_test_patients)]] ;
+echo "${print_prefix} Total patients sinked to fhir ---> ${total_patients_sinked_fhir}"
+if [[ ${total_patients_sinked_fhir} == ${total_test_patients} ]] ;
 then
   echo "${print_prefix} BATCH MODE WITH FHIR SERVER SINK EXECUTED SUCCESSFULLY"
 else
   echo "${print_prefix} BATCH MODE WITH FHIR SERVER SINK TEST FAILED"
+  exit 1
 fi
 echo "${print_prefix} END!!"
--- e2e-tests/run.sh
+++ e2e-tests/run.sh
@@ -1,2 +1,4 @@
-#!/bin/bash
-sh e2e-tests/batch-mode-tests.sh
+#!/usr/bin/env bash
+
+chmod +x ./e2e-tests/batch-mode-tests.sh
+./e2e-tests/batch-mode-tests.sh
