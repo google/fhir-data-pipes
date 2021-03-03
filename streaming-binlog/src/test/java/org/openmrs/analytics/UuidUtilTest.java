a0331950c565fe28e2365c2418dda5d89c090e5e
cliff <gitacliff48@gmail.com>
2021-03-03 11:57:27 +0300

    UUIDS:Fixing UuidUtilTest

--- streaming-binlog/src/test/java/org/openmrs/analytics/UuidUtilTest.java
+++ streaming-binlog/src/test/java/org/openmrs/analytics/UuidUtilTest.java
@@ -15,10 +15,13 @@
 
 package org.openmrs.analytics;
 
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 
 import java.beans.PropertyVetoException;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -36,41 +39,64 @@ public class UuidUtilTest extends TestCase {
 	@Mock
 	private JdbcConnectionUtil jdbcConnectionUtil;
 	
+	@Mock
+	private Statement statement;
+	
+	@Mock
+	private ResultSet resultset;
+	
 	private EventConfiguration config;
 	
 	private Map<String, String> payLoad;
 	
+	private String uuid;
+	
+	private String table;
+	
+	private String keyColumn;
+	
+	private String keyValue;
+	
 	@Before
 	public void beforeTestCase() throws Exception {
-		
-		when(jdbcConnectionUtil.getJdbcDriverClass()).thenReturn("com.mysql.cj.jdbc.Driver");
-		when(jdbcConnectionUtil.getJdbcUrl()).thenReturn("jdbc:mysql://localhost:3308/openmrs");
-		when(jdbcConnectionUtil.getDbUser()).thenReturn("root");
-		when(jdbcConnectionUtil.getDbPassword()).thenReturn("debezium");
-		when(jdbcConnectionUtil.getInitialPoolSize()).thenReturn(10);
-		when(jdbcConnectionUtil.getDbcMaxPoolSize()).thenReturn(50);
-		
 		payLoad = new HashMap<>();
 		config = new EventConfiguration();
 		
 		payLoad.put("uuid", "1");
 		payLoad.put("sex", "male");
+		payLoad.put("patient_id", "5");
 		
 		config.setParentTable("person");
 		config.setParentForeignKey("person_id");
 		config.setChildPrimaryKey("patient_id");
+		
+		uuid = "1296b0dc-440a-11e6-a65c-00e04c680037";
+		table = config.getParentTable();
+		keyColumn = config.getParentForeignKey();
+		keyValue = payLoad.get(config.getChildPrimaryKey()).toString();
+		
+		String sql = String.format("SELECT uuid FROM %s WHERE %s = %s", table, keyColumn, keyValue);
+		
+		lenient().when(jdbcConnectionUtil.getJdbcDriverClass()).thenReturn("com.mysql.cj.jdbc.Driver");
+		lenient().when(jdbcConnectionUtil.getJdbcUrl()).thenReturn("jdbc:mysql://localhost:3308/openmrs");
+		lenient().when(jdbcConnectionUtil.getDbUser()).thenReturn("root");
+		lenient().when(jdbcConnectionUtil.getDbPassword()).thenReturn("debezium");
+		lenient().when(jdbcConnectionUtil.getInitialPoolSize()).thenReturn(10);
+		lenient().when(jdbcConnectionUtil.getDbcMaxPoolSize()).thenReturn(50);
+		
+		when(jdbcConnectionUtil.createStatement()).thenReturn(statement);
+		when(statement.executeQuery(sql)).thenReturn(resultset);
+		when(resultset.next()).thenReturn(true).thenReturn(false);
+		when(resultset.getString("uuid")).thenReturn(uuid);
 	}
 	
 	@Test
 	public void shouldCallGetUuid() throws ClassNotFoundException, PropertyVetoException, SQLException {
 		
 		UuidUtil uuidUtil = new UuidUtil(jdbcConnectionUtil);
-		
-		String uuid = uuidUtil.getUuid(config.getParentTable(), config.getParentForeignKey(),
-		    payLoad.get(config.getChildPrimaryKey()).toString());
+		String uuid = uuidUtil.getUuid(table, keyColumn, keyValue);
 		
 		assertNotNull(uuid);
-		
 		assertEquals(uuid, "1296b0dc-440a-11e6-a65c-00e04c680037");
 		
 	}
