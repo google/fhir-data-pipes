bd5462ffce5654199c1b316229dea5e6c1e59d9f
Omar Ismail <omarismail@google.com>
2021-03-15 21:08:30 -0400

    added suggested comments

--- utils/create_fhir_store.sh
+++ utils/create_fhir_store.sh
@@ -22,47 +22,44 @@ if [[ $# -ne 4 ]]; then
   exit 1
 fi
 
-# Function that creates dataset and checks if the dataset already exist
-function create_dataset
-{
-  # Run command passed in
+#######################################
+## Function that creates dataset and checks if the dataset already exist
+# Arguments:
+#   gcloud command to create a dataset
+#######################################
+function create_dataset() {
   output=`$@ 2>&1`
-  
-  # Store the returned code
+
   code=$?
-  
-  # If operation succeeded, output that dataset was created 
+
   if [[ $code -eq 0 ]]; then
-      echo 'Created dataset'
-  else   
+    echo "Created dataset"
+  else
     # If the dataset already exists, let user know
-    if [[ $output == *'already exists'* ]]; then
-          echo 'Dataset already exists'
-    
-    # For any other error, output the error and exit
+    if [[ "${output}" == *"already exists"* ]]; then
+      echo "Dataset already exists"
+
     else
-          echo $output
-          exit -1
+      echo "ERROR ${output}" >&2
+      exit 1
     fi
-  fi  
+  fi
 }
 
-# Creating GCP Healthcare dataset 
 echo "Creating GCP Healthcare dataset.."
 create_dataset "gcloud healthcare datasets create ${3} --location=${2}"
 
-# Creating FHIR store
+echo "Creating FHIR Store..."
 curl --request POST -H "Authorization: Bearer $(gcloud auth print-access-token)" \
   -H "Content-Type: application/fhir+json; charset=utf-8" \
   "https://healthcare.googleapis.com/v1/projects/${1}/locations/${2}/datasets/${3}/fhirStores?fhirStoreId=${4}" \
   --data '{"version":"R4", "enableUpdateCreate":true,"disableReferentialIntegrity":true}' \
   -w '\nFHIR store creation status code: %{http_code}\n'
 
-# Creating BQ dataset
 echo "Creating BQ dataset..."
 create_dataset "bq --location=${2} mk --dataset ${1}:${3}"
 
-# Setting up BQ streaming
+echo "Setting up BQ streaming..."
 curl -X PATCH \
   -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
   -H "Content-Type: application/json; charset=utf-8" \
@@ -80,3 +77,5 @@ curl -X PATCH \
   }" \
   "https://healthcare.googleapis.com/v1/projects/${1}/locations/${2}/datasets/${3}/fhirStores/${4}?updateMask=streamConfigs" \
   -w '\nFHIR store BQ streaming set up status code: %{http_code}\n'
+
+echo "Complete!!"
