fd160a045241935f9b8bc7abb28cc7a3287d7d2b
Bashir Sadjad <bashir.sadjad@gmail.com>
2022-01-28 23:34:54 -0500

    regenerated test parquet files with location

--- dwh/validate_indicators.sh
+++ dwh/validate_indicators.sh
@@ -82,34 +82,38 @@ function validate() {
 
 FAILED=""
 # PVLS counts
-validate "Suppressed, non-suppressed, none, male_25 numbers are" "4,370,0,0" 3
+validate "Suppressed, non-suppressed, none, male_25 numbers are" "8,751,0,2" 3
 # PVLS ratio
 validate "Suppressed, non-suppressed, none, male_25 ratios are" \
-  "0.0107,0.989,0,0" 4
+  "0.0105,0.989,0,0.00264" 4
 # TX_NEW counts
 # TODO validate these manually by querying the DB
-validate "TX_NEW, non-TX_NEW, none, male_25 numbers are" "60,218,0,0" 6
+validate "TX_NEW, non-TX_NEW, none, male_25 numbers are" "95,436,0,24" 6
 # TX_NEW ratio
-validate "TX_NEW, non-TX_NEW, none, male_25 ratios are" "0.216,0.784,0,0" 7
+validate "TX_NEW, non-TX_NEW, none, male_25 ratios are" "0.179,0.821,0,0.0452" 7
 # TB_STAT counts
-validate "TB_STAT, non-TB_STAT, none, male_25 numbers are" "49,229,0,0" 9
+validate "TB_STAT, non-TB_STAT, none, male_25 numbers are" "86,445,0,29" 9
 # TB_STAT ratio
-validate "TB_STAT, non-TB_STAT, none, male_25 ratios are" "0.176,0.824,0,0" 10
+validate "TB_STAT, non-TB_STAT, none, male_25 ratios are" \
+  "0.162,0.838,0,0.0546" 10
 # TX_CURR counts
-validate "TX_CURR, non-TX_CURR, none, male_25 numbers are" "73,205,0,0" 12
+validate "TX_CURR, non-TX_CURR, none, male_25 numbers are" "116,415,0,29" 12
 # TX_CURR ratio
-validate "TX_CURR, non-TX_CURR, none, male_25 ratios are" "0.263,0.737,0,0" 13
+validate "TX_CURR, non-TX_CURR, none, male_25 ratios are" \
+  "0.218,0.782,0,0.0546" 13
 # TB_ART counts
-validate "TB_ART, non-TB_ART, none, male_25 numbers are" "25,253,0,0" 15
+validate "TB_ART, non-TB_ART, none, male_25 numbers are" "43,488,0,13" 15
 # TB_ART ratio
-validate "TB_ART, non-TB_ART, none, male_25 ratios are" "0.0899,0.91,0,0" 16
+validate "TB_ART, non-TB_ART, none, male_25 ratios are" \
+  "0.081,0.919,0,0.0245" 16
 # TB_PREV counts
-validate "TB_PREV, non-TB_PREV, none, male_25 numbers are" "35,3.19e+03,0,0" 18
+validate "TB_PREV, non-TB_PREV, none, male_25 numbers are" "58,6.34e+03,0,8" 18
 # TB_PREV ratio
-validate "TB_PREV, non-TB_PREV, none, male_25 ratios are" "0.0109,0.989,0,0" 19
+validate "TB_PREV, non-TB_PREV, none, male_25 ratios are" \
+  "0.00906,0.991,0,0.00125" 19
 
 # TX_TB counts
-validate "TX_TB, non-TX_TB, none, male_25 numbers are" "0,3.22e+03,0,0" 21
+validate "TX_TB, non-TX_TB, none, male_25 numbers are" "0,6.4e+03,0,0" 21
 # TX_TB ratio
 validate "TX_TB, non-TX_TB, none, male_25 ratios are" "0,1,0,0" 22
 
