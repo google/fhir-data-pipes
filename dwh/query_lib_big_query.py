e1deaca50df967b33a54b40e26f7ed30dbd44b44
Guru Devanla <gurud@google.com>
2022-05-16 13:50:39 +0000

    formatting

--- dwh/query_lib_big_query.py
+++ dwh/query_lib_big_query.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 from query_lib import PatientQuery
 
-_DATE_VALUE_SEPARATOR = ','
+_DATE_VALUE_SEPARATOR = ","
 
 try:
     from google.cloud import bigquery
@@ -16,14 +16,19 @@ def _build_in_list_with_quotes(values: tp.Iterable[tp.Any]):
     """
     Builds the `in` operand for a where clause
     """
-    return ','.join(('"{}"'.format(x) for x in values))
+    return ",".join(('"{}"'.format(x) for x in values))
+
 
 class _BigQueryEncounterConstraints:
     """Encounter constraints helper class that will be set up for querying
     BigQuery."""
 
-    def __init__(self, locationId: tp.List[str] = None,
-      typeSystem: str = None, typeCode: tp.List[str] = None):
+    def __init__(
+        self,
+        locationId: tp.List[str] = None,
+        typeSystem: str = None,
+        typeCode: tp.List[str] = None,
+    ):
         self._location_id = locationId
         self._type_system = typeSystem
         self._type_code = typeCode
@@ -54,23 +59,30 @@ class _BigQueryEncounterConstraints:
             clause_type_codes = "C.code in ({})".format(
                 _build_in_list_with_quotes(self._type_code)
             )
-        where_clause = ' and '.join(
+        where_clause = " and ".join(
             x for x in [clause_location_id, clause_type_system, clause_type_codes] if x
         )
         if where_clause:
-            return ' where {} '.format(where_clause)
-        return ''
+            return " where {} ".format(where_clause)
+        return ""
 
 
 class _BigQueryObsConstraints:
     """Observation constraints helper class for querying Big Query."""
 
-    def __init__(self, code: str, values: tp.List[str] = None, value_sys: str = None,
-      min_value: float = None, max_value: float = None,
-      min_time: str = None, max_time: str = None) -> None:
+    def __init__(
+        self,
+        code: str,
+        values: tp.List[str] = None,
+        value_sys: str = None,
+        min_value: float = None,
+        max_value: float = None,
+        min_time: str = None,
+        max_time: str = None,
+    ) -> None:
 
         self._code = code
-        self._sys_str = '="{}"'.format(value_sys) if value_sys else 'IS NULL'
+        self._sys_str = '="{}"'.format(value_sys) if value_sys else "IS NULL"
         self._values = values
         self._min_time = min_time
         self._max_time = max_time
@@ -83,13 +95,13 @@ class _BigQueryObsConstraints:
         Build time constraints for Observation queries
         """
         if not min_time and not max_time:
-            return ' TRUE '
+            return " TRUE "
         conditions = []
         if min_time:
             conditions.append('O.effective.dateTime >= "{}"'.format(min_time))
         if max_time:
             conditions.append('O.effective.dateTime <= "{}"'.format(max_time))
-        return ' AND '.join(conditions)
+        return " AND ".join(conditions)
 
     def where_clause(self):
         """Build obs criteria."""
@@ -98,16 +110,20 @@ class _BigQueryObsConstraints:
         # We don't need to filter coding.system as it is already done in
         # flattening.
         if self._values:
-            codes_str = ','.join(['"{}"'.format(v) for v in self._values])
-            conditions.append('OVC.code IN ({})'.format(codes_str))
+            codes_str = ",".join(['"{}"'.format(v) for v in self._values])
+            conditions.append("OVC.code IN ({})".format(codes_str))
             # TODO(gdevanla): This is already applied as part of patient._code_system
             # conditions.append('OVC.system {}'.format(self._sys_str))
         elif self._min_value or self._max_value:
             if self._min_value:
-                conditions.append(' O.value.quantity.value >= {} '.format(self._min_value))
+                conditions.append(
+                    " O.value.quantity.value >= {} ".format(self._min_value)
+                )
             if self._max_value:
-                conditions.append(' O.value.quantity.value <= {} '.format(self._max_value))
-        return '({})'.format(' AND '.join(conditions))
+                conditions.append(
+                    " O.value.quantity.value <= {} ".format(self._max_value)
+                )
+        return "({})".format(" AND ".join(conditions))
 
 
 class _BigQueryPatientQuery(PatientQuery):
@@ -170,7 +186,7 @@ class _BigQueryPatientQuery(PatientQuery):
             base_url=base_url,
             data_set=bq_dataset,
             where=where_clause,
-            sample_count='' if sample_count is None else 'LIMIT ' + str(sample_count),
+            sample_count="" if sample_count is None else "LIMIT " + str(sample_count),
         )
         return sql
 
@@ -242,16 +258,20 @@ class _BigQueryPatientQuery(PatientQuery):
         # TODO(gdevanla): Yet to add encounter constraints
 
         code_coding_system_str = (
-            ' OC.system is null '
+            " OC.system is null "
             if not self._code_system
             else ' OC.system = "{}" '.format(self._code_system)
         )
-        value_codeable_coding_system_str = ' O.value is null or ' + (
-            ' OVC.system is null '
+        value_codeable_coding_system_str = " O.value is null or " + (
+            " OVC.system is null "
             if not self._code_system
-            else ' (OVC.system is null or  OVC.system = "{}") '.format(self._code_system)
+            else ' (OVC.system is null or  OVC.system = "{}") '.format(
+                self._code_system
+            )
         )
-        value_codeable_coding_system_str = " ({}) ".format(value_codeable_coding_system_str)
+        value_codeable_coding_system_str = " ({}) ".format(
+            value_codeable_coding_system_str
+        )
 
         all_obs_constraints = self._all_obs_constraints()
 
@@ -260,7 +280,7 @@ class _BigQueryPatientQuery(PatientQuery):
             code_coding_system=code_coding_system_str,
             value_codeable_coding_system=value_codeable_coding_system_str,
             all_obs_constraints=all_obs_constraints,
-            sample_count='' if sample_count is None else ' LIMIT ' + str(sample_count),
+            sample_count="" if sample_count is None else " LIMIT " + str(sample_count),
             base_url=base_url,
             encounter_where_clause=self._enc_constraint.where_clause(),
         )
@@ -270,19 +290,19 @@ class _BigQueryPatientQuery(PatientQuery):
 
         if not self._code_constraint:
             if self._include_all_codes:
-                return 'TRUE'
-            return 'FALSE'
+                return "TRUE"
+            return "FALSE"
 
-        constraints_str = ' OR '.join(
+        constraints_str = " OR ".join(
             [
                 obs_constraint.where_clause()
                 for obs_constraint in self._code_constraint.values()
             ]
         )
         if not self._include_all_codes:
-            return ' ({}) '.format(constraints_str)
+            return " ({}) ".format(constraints_str)
 
-        others_str = ' AND '.join(
+        others_str = " AND ".join(
             ['OC.code != "{}"'.format(code) for code in self._code_constraint]
             + [
                 self._obs_constraints_class.time_constraint(
@@ -290,7 +310,7 @@ class _BigQueryPatientQuery(PatientQuery):
                 )
             ]
         )
-        return ' (({}) OR ({})) '.format(constraints_str, others_str)
+        return " (({}) OR ({})) ".format(constraints_str, others_str)
 
     def get_patient_encounter_view(
         self,
@@ -301,7 +321,7 @@ class _BigQueryPatientQuery(PatientQuery):
 
         sql = self._build_encounter_query(
             bq_dataset=self._bq_dataset,
-            table_name='Encounter',
+            table_name="Encounter",
             base_url=base_url,
             force_location_type_columns=force_location_type_columns,
             sample_count=sample_count,
@@ -324,15 +344,15 @@ class _BigQueryPatientQuery(PatientQuery):
         with bigquery.Client() as client:
             patient_obs_enc = client.query(sql).to_dataframe()
             col_map = (
-                ('last_value', 'max_date_value'),
-                ('first_value', 'min_date_value'),
-                ('last_value_code', 'max_date_value_code'),
-                ('first_value_code', 'min_date_value_code'),
+                ("last_value", "max_date_value"),
+                ("first_value", "min_date_value"),
+                ("last_value_code", "max_date_value_code"),
+                ("first_value_code", "min_date_value_code"),
             )
 
             for dest_col, source_col in col_map:
                 patient_obs_enc[dest_col] = patient_obs_enc[source_col].apply(
-                    lambda x: None if x is None else x.split(',')[1]
+                    lambda x: None if x is None else x.split(",")[1]
                 )
             patient_obs_enc.drop(columns=[col[1] for col in col_map], inplace=True)
             return patient_obs_enc
