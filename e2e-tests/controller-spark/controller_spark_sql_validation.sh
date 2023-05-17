2f6c291f7c5a7c10d877f76bd233361ca8bc757b
Atul Kumar <atulai@google.com>
2023-05-17 11:52:10 +0000

    Added scenario to test updated resource post dockerized pipeline run

--- e2e-tests/controller-spark/controller_spark_sql_validation.sh
+++ e2e-tests/controller-spark/controller_spark_sql_validation.sh
@@ -15,8 +15,8 @@
 # limitations under the License.
 
 # Example usage:
-#   ./single_machine_deployment_validation.sh ./ dwh
-#   ./single_machine_deployment_validation.sh ./ dwh --use_docker_network
+#   ./controller_spark_sql_validation.sh ./ dwh
+#   ./controller_spark_sql_validation.sh ./ dwh --use_docker_network
 
 set -e
 
@@ -27,8 +27,8 @@ function usage() {
   echo "This script validates if number of resources captured through"
   echo "parquet files match what is stored in the source FHIR server"
   echo
-  echo " usage: ./single_machine_deployment_validation.sh  HOME_DIR  PARQUET_SUBDIR  [OPTIONS] "
-  echo "    HOME_DIR          Path where e2e-tests/dingle-machine directory is. Directory MUST"
+  echo " usage: ./controller_spark_sql_validation.sh  HOME_DIR  PARQUET_SUBDIR  [OPTIONS] "
+  echo "    HOME_DIR          Path where e2e-tests/controller-spark directory is. Directory MUST"
   echo "                      contain the parquet tools jar as well as subdirectory"
   echo "                      of parquet file output"
   echo "    PARQUET_SUBDIR    Subdirectory name under HOME_DIR containing"
@@ -50,7 +50,7 @@ function validate_args() {
   fi
 
   echo "Checking if the Parquet-tools JAR exists..."
-  if compgen -G "${1}/parquet-tools*.jar" > /dev/null; then
+  if [[ -n $(find "${1}" -name parquet-tools*.jar) ]]; then
     echo "Parquet-tools JAR exists in ${1}"
   else
     echo "Parquet-tools JAR not found in ${1}"
@@ -65,7 +65,7 @@ function validate_args() {
 #   anything that needs printing
 #################################################
 function print_message() {
-  local print_prefix="E2E TEST FOR SINGLE MACHINE DEPLOYMENT:"
+  local print_prefix="E2E TEST FOR CONTROLLER SPARK DEPLOYMENT:"
   echo "${print_prefix} $*"
 }
 
@@ -78,7 +78,7 @@ function print_message() {
 #   PIPELINE_CONTROLLER_URL
 #   THRIFTSERVER_URL
 # Arguments:
-#   Path where e2e-tests/single-machine directory is. Directory contains parquet tools jar as
+#   Path where e2e-tests/controller-spark directory is. Directory contains parquet tools jar as
 #      well as subdirectory of parquet file output
 #   Subdirectory name under HOME_DIR containing parquet files.
 #      Example: dwh
@@ -114,19 +114,20 @@ function fhir_source_query() {
   local fhir_url_extension="/fhir"
 
   curl -L -X GET -u $fhir_username:$fhir_password --connect-timeout 5 --max-time 20 \
-  "${SOURCE_FHIR_SERVER_URL}${fhir_url_extension}/Patient${query_param}" 2>/dev/null >>"${HOME_PATH}/${PARQUET_SUBDIR}/patients.json"
+  "${SOURCE_FHIR_SERVER_URL}${fhir_url_extension}/Patient${query_param}" 2>/dev/null \
+  >"${HOME_PATH}/${PARQUET_SUBDIR}/patients.json"
   TOTAL_TEST_PATIENTS=$(jq '.total' "${HOME_PATH}/${PARQUET_SUBDIR}/patients.json")
   print_message "Total FHIR source test patients ---> ${TOTAL_TEST_PATIENTS}"
 
   curl -L -X GET -u $fhir_username:$fhir_password --connect-timeout 5 --max-time 20 \
     "${SOURCE_FHIR_SERVER_URL}${fhir_url_extension}/Encounter${query_param}" \
-    2>/dev/null >>"${HOME_PATH}/${PARQUET_SUBDIR}/encounters.json"
+    2>/dev/null >"${HOME_PATH}/${PARQUET_SUBDIR}/encounters.json"
   TOTAL_TEST_ENCOUNTERS=$(jq '.total' "${HOME_PATH}/${PARQUET_SUBDIR}/encounters.json")
   print_message "Total FHIR source test encounters ---> ${TOTAL_TEST_ENCOUNTERS}"
 
   curl -L -X GET -u $fhir_username:$fhir_password --connect-timeout 5 --max-time 20 \
     "${SOURCE_FHIR_SERVER_URL}${fhir_url_extension}/Observation${query_param}" \
-    2>/dev/null >>"${HOME_PATH}/${PARQUET_SUBDIR}/obs.json"
+    2>/dev/null >"${HOME_PATH}/${PARQUET_SUBDIR}/obs.json"
   TOTAL_TEST_OBS=$(jq '.total' "${HOME_PATH}/${PARQUET_SUBDIR}/obs.json")
   print_message "Total FHIR source test obs ---> ${TOTAL_TEST_OBS}"
 }
@@ -139,7 +140,7 @@ function fhir_source_query() {
 #   isFullRun: flag to indicate whether to start full or incremental run.
 #######################################################################
 function run_pipeline() {
-  isFullRun=$1
+  local isFullRun=$1
   curl --location --request POST "${PIPELINE_CONTROLLER_URL}/run?isFullRun=${isFullRun}" \
   --connect-timeout 5 \
   --header 'Content-Type: application/json' \
@@ -160,11 +161,11 @@ function run_pipeline() {
 #   isIncremental: flag to differentiate behavior between incremental and batch runs.
 #######################################################################
 function check_parquet() {
-  isIncremental=$1
-  runtime="5 minute"
-  end_time=$(date -ud "$runtime" +%s)
-  output="${HOME_PATH}/${PARQUET_SUBDIR}"
-  timeout=true
+  local isIncremental=$1
+  local runtime="5 minute"
+  local end_time=$(date -ud "$runtime" +%s)
+  local output="${HOME_PATH}/${PARQUET_SUBDIR}"
+  local timeout=true
 
   if [[ "${isIncremental}" == "true" ]]
   then
@@ -173,19 +174,18 @@ function check_parquet() {
     TOTAL_TEST_PATIENTS=$((2*TOTAL_TEST_PATIENTS + 1))
     TOTAL_TEST_ENCOUNTERS=$((2*TOTAL_TEST_ENCOUNTERS))
     TOTAL_TEST_OBS=$((2*TOTAL_TEST_OBS))
-    print_message "Full batch run started."
-  else
-    print_message "Incremental run started."
   fi
 
   while [[ $(date -u +%s) -le $end_time ]]
   do
     # check whether output directory has started receiving parquet files.
-    if [ "$(ls -A $output)" ]
+    if [[ "$(ls -A $output)" ]]
     then
-      total_patients=$(java -jar ./parquet-tools-1.11.1.jar rowcount "${output}/*/Patient/" | awk '{print $3}')
-      total_encounters=$(java -jar ./parquet-tools-1.11.1.jar rowcount "${output}/*/Encounter/" | awk '{print $3}')
-      total_observations=$(java -jar ./parquet-tools-1.11.1.jar rowcount "${output}/*/Observation/" | awk '{print $3}')
+      local total_patients=$(java -jar ./parquet-tools-1.11.1.jar rowcount "${output}/*/Patient/" | awk '{print $3}')
+      local total_encounters=$(java -jar ./parquet-tools-1.11.1.jar rowcount "${output}/*/Encounter/" \
+       | awk '{print $3}')
+      local total_observations=$(java -jar ./parquet-tools-1.11.1.jar rowcount "${output}/*/Observation/" \
+       | awk '{print $3}')
 
       print_message "Total patients: $total_patients"
       print_message "Total encounters: $total_encounters"
@@ -206,7 +206,7 @@ function check_parquet() {
   if [[ "${timeout}" == "true" ]]
   then
     print_message "Could not validate parquet files."
-    exit 5
+    exit 2
   fi
 }
 
@@ -237,6 +237,33 @@ function add_resource() {
 }
 
 #######################################################################
+# Function to update resource on hapi server so that we can have some
+# difference for incremental run.
+# Globals:
+#   SOURCE_FHIR_SERVER_URL
+#   PATIENT_ID
+#######################################################################
+function update_resource() {
+  local fhir_username="hapi"
+  local fhir_password="hapi"
+  local fhir_url_extension="/fhir"
+
+  # Get patient id which we wish to modify.
+  PATIENT_ID=$(curl -X GET -H "Content-Type: application/json; charset=utf-8" -u $fhir_username:$fhir_password \
+  --connect-timeout 5 --max-time 20 "${SOURCE_FHIR_SERVER_URL}${fhir_url_extension}/Patient" \
+   | jq '.entry' | jq -r '.[0].resource.id')
+
+  print_message "Patient id which is being updated: ${PATIENT_ID}."
+
+  # Update family name of the patient.
+  curl -X PATCH -H "Content-Type: application/json-patch+json; charset=utf-8" -u $fhir_username:$fhir_password \
+  --connect-timeout 5 --max-time 20 --data '[{ "op": "replace", "path": "/name/0/family", "value": "Anderson" }]' \
+  "${SOURCE_FHIR_SERVER_URL}${fhir_url_extension}/Patient/${PATIENT_ID}"
+
+  print_message "Patient ${PATIENT_ID} updated successfully."
+}
+
+#######################################################################
 # Function to validate automatic creation of resource tables on pipeline run.
 # Globals:
 #   THRIFTSERVER_URL
@@ -246,23 +273,23 @@ function validate_resource_tables() {
   --outputformat=csv2 >>hive_resource_tables.csv
 
   # Check for snapshot tables.
-  if grep -q patient_ hive_resource_tables.csv && grep -q encounter_ hive_resource_tables.csv \
-      && grep -q observation_ hive_resource_tables.csv
+  if [[ $(grep patient_ hive_resource_tables.csv) && $(grep encounter_ hive_resource_tables.csv) \
+      && $(grep observation_ hive_resource_tables.csv) ]]
   then
     print_message "Snapshot tables creation verified successfully."
   else
     print_message "Snapshot tables verification failed."
-    exit 2
+    exit 3
   fi
 
   # Check for canonical tables.
-  if grep -w patient hive_resource_tables.csv && grep -w encounter hive_resource_tables.csv \
-      && grep -w observation hive_resource_tables.csv
+  if [[ $(grep -w patient hive_resource_tables.csv) && $(grep -w encounter hive_resource_tables.csv) \
+      && $(grep -w observation hive_resource_tables.csv) ]]
   then
     print_message "Canonical tables creation verified successfully."
   else
     print_message "Canonical tables verification failed."
-    exit 3
+    exit 4
   fi
 }
 
@@ -284,12 +311,35 @@ function validate_resource_tables_data() {
   beeline -u "jdbc:hive2://${THRIFTSERVER_URL}" -n hive -e "${query}" \
     --outputformat=csv2 >>hive_resource_tables_data.csv
 
-  if grep -q 265 hive_resource_tables_data.csv
+  if [[ $(grep -w 265 hive_resource_tables_data.csv) ]]
   then
     print_message "Resource tables data verified successfully."
   else
     print_message "Resource tables data verification failed."
-    exit 4
+    exit 5
+  fi
+}
+
+##############################################################
+# Function to validate updated resource on pipeline run.
+# When fetched from hapi server for patient id 13526, their given name comes as Shalon513
+# and we have updated that to Sharon513, so we should verify Sharon513 in the patient table.
+# Globals:
+#   THRIFTSERVER_URL
+#   PATIENT_ID
+##############################################################
+function validate_updated_resource() {
+  # Count all viral-load observations.
+  local query="SELECT p.name.family FROM patient AS p where p.id = ${PATIENT_ID};"
+  beeline -u "jdbc:hive2://${THRIFTSERVER_URL}" -n hive -e "${query}" \
+    --outputformat=csv2 >>patient_data.csv
+
+  if [[ $(grep -w Anderson patient_data.csv) ]]
+  then
+    print_message "Updated patient data verified successfully."
+  else
+    print_message "Updated patient data verification failed."
+    exit 6
   fi
 }
 
@@ -304,11 +354,13 @@ check_parquet false
 clear
 
 add_resource
+update_resource
 # Incremental run.
 run_pipeline false
 check_parquet true
 
 validate_resource_tables
 validate_resource_tables_data
+validate_updated_resource
 
 print_message "END!!"
