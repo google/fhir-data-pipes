3df126ff9a33296812c57da947040fa41cc6ffc4
Bashir Sadjad <bashir.sadjad@gmail.com>
2023-10-18 18:43:27 +0000

    review comments

--- docker/config/views/Encounter_flat.sql
+++ docker/config/views/Encounter_flat.sql
@@ -1,9 +1,10 @@
 CREATE OR REPLACE VIEW Encounter_flat AS
-SELECT E.id AS enc_id, E.status, ETC.code, E.subject.PatientId AS patient_id,
+SELECT E.id AS enc_id, E.status, ETC.system AS type_sys,
+  ETC.code AS type_code, E.subject.PatientId AS patient_id,
   EP.individual.practitionerId AS practitioner_id,
   EL.location.locationId AS location_id,
   E.serviceProvider.organizationId AS service_org_id,
-  E.period.start, E.period.end
+  E.period.start, E.period.end, E.episodeOfCare.EpisodeOfCareId
 FROM Encounter AS E LATERAL VIEW OUTER explode(type) AS ET
   LATERAL VIEW OUTER explode(ET.coding) AS ETC
   LATERAL VIEW OUTER explode(E.participant) AS EP
