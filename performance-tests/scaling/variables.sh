a751fa93578034a7a417496a091855f97c525587
Jakub Adámek <jakuba@google.com>
2024-04-04 16:15:06 +0000

    patient counts

--- performance-tests/scaling/variables.sh
+++ performance-tests/scaling/variables.sh
@@ -1,6 +1,6 @@
 export PATH="/google/data/ro/projects/java-platform/linux-amd64/jdk-17-latest/bin:$PATH"
-# export PATIENTS="79_patients"
-export PATIENTS="7885_patients"
+# 79,768,7885,79370,791562
+export PATIENTS="768_patients"
 export DB_INSTANCE="pipeline-scaling-1"
 
 export VM_INSTANCE="pipeline-scaling-20240318-090525"
