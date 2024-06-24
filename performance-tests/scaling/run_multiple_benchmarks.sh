1e1fcd07bda5868a32e13b077b9c467babd44875
Jakub Adámek <jakuba@google.com>
2024-06-24 06:55:48 +0000

    Add multiple batch sizes

--- performance-tests/scaling/run_multiple_benchmarks.sh
+++ performance-tests/scaling/run_multiple_benchmarks.sh
@@ -16,22 +16,29 @@ if [[ -n "$FHIR_SERVER_URL" ]]; then
   echo "ERROR: Comment out FHIR_SERVER_URL in config.sh if running multiple"
   exit 1
 fi
+if [[ -n "$BATCH_SIZE" ]]; then
+  echo "ERROR: Comment out BATCH_SIZE in config.sh if running multiple"
+  exit 1
+fi
 
 set -e # Fail on errors.
 set -x # Show each command.
 set -o nounset
 
-for p in $MULTIPLE_PATIENTS; do
-  for j in $MULTIPLE_JDBC_MODE; do
-    for server in $MULTIPLE_FHIR_SERVER_URL; do
-      for f in $MULTIPLE_FHIR_ETL_RUNNER; do
-        export PATIENTS=$p
-        export JDBC_MODE=$j
-        export FHIR_ETL_RUNNER=$f
-        export FHIR_SERVER_URL=$server
-        ./setup_google3.sh
-        sleep 15
-        ./upload_download.sh
+for batch in $MULTIPLE_BATCH_SIZE; do
+  for p in $MULTIPLE_PATIENTS; do
+    for j in $MULTIPLE_JDBC_MODE; do
+      for server in $MULTIPLE_FHIR_SERVER_URL; do
+        for f in $MULTIPLE_FHIR_ETL_RUNNER; do
+          export PATIENTS=$p
+          export JDBC_MODE=$j
+          export FHIR_ETL_RUNNER=$f
+          export FHIR_SERVER_URL=$server
+          export BATCH_SIZE=$batch
+          ./setup_google3.sh
+          sleep 15
+          ./upload_download.sh
+        done
       done
     done
   done
