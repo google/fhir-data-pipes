fba3c9a189a2ad07146f6dca98b9f063d64b040e
Jakub Adámek <jakuba@google.com>
2024-07-06 13:19:22 +0000

    Make Flink and Dataflow runners multiple

--- performance-tests/scaling/run_multiple_benchmarks.sh
+++ performance-tests/scaling/run_multiple_benchmarks.sh
@@ -20,26 +20,40 @@ if [[ -n "$BATCH_SIZE" ]]; then
   echo "ERROR: Comment out BATCH_SIZE in config.sh if running multiple"
   exit 1
 fi
+if [[ -n "$FLINK_PARALLEL" ]]; then
+  echo "ERROR: Comment out FLINK_PARALLEL in config.sh if running multiple"
+  exit 1
+fi
+if [[ -n "$DATAFLOW_WORKERS" ]]; then
+  echo "ERROR: Comment out DATAFLOW_WORKERS in config.sh if running multiple"
+  exit 1
+fi
 
 set -e # Fail on errors.
 set -x # Show each command.
 set -o nounset
 
-for batch in $MULTIPLE_BATCH_SIZE; do
-  for p in $MULTIPLE_PATIENTS; do
-    for j in $MULTIPLE_JDBC_MODE; do
-      for server in $MULTIPLE_FHIR_SERVER_URL; do
-        for f in $MULTIPLE_FHIR_ETL_RUNNER; do
-          export PATIENTS=$p
-          export JDBC_MODE=$j
-          export FHIR_ETL_RUNNER=$f
-          export FHIR_SERVER_URL=$server
-          export BATCH_SIZE=$batch
-          if [ "$ENABLE_SETUP_GOOGLE3" = true ]; then
-            ./setup_google3.sh
-            sleep 15
-          fi
-          ./upload_download.sh
+for dataflow in $MULTIPLE_DATAFLOW_WORKERS; do
+  for flink in $MULTIPLE_FLINK_PARALLEL; do
+    for batch in $MULTIPLE_BATCH_SIZE; do
+      for p in $MULTIPLE_PATIENTS; do
+        for j in $MULTIPLE_JDBC_MODE; do
+          for server in $MULTIPLE_FHIR_SERVER_URL; do
+            for f in $MULTIPLE_FHIR_ETL_RUNNER; do
+              export PATIENTS=$p
+              export JDBC_MODE=$j
+              export FHIR_ETL_RUNNER=$f
+              export FHIR_SERVER_URL=$server
+              export BATCH_SIZE=$batch
+              export FLINK_PARALLEL=$flink
+              export DATAFLOW_WORKERS=$dataflow
+              if [ "$ENABLE_SETUP_GOOGLE3" = true ]; then
+                ./setup_google3.sh
+                sleep 15
+              fi
+              ./upload_download.sh
+            done
+          done
         done
       done
     done
