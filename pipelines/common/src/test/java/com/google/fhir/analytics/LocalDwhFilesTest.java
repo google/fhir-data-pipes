4dee9991544be856368f2e0ca207b33c02dfbacd
Meet Mehta <meetmehta1198@gmail.com>
2024-11-25 20:42:23 -0500

    Added import for Assume

--- pipelines/common/src/test/java/com/google/fhir/analytics/LocalDwhFilesTest.java
+++ pipelines/common/src/test/java/com/google/fhir/analytics/LocalDwhFilesTest.java
@@ -38,6 +38,7 @@ import org.apache.beam.sdk.io.FileSystems;
 import org.apache.beam.sdk.io.fs.ResolveOptions.StandardResolveOptions;
 import org.apache.beam.sdk.io.fs.ResourceId;
 import org.apache.commons.lang3.SystemUtils;
+import org.junit.Assume;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
